1da36106f1720bd8ff53d20c8605441d336eed0f
00Dre00 <darkpunisher.andrea@gmail.com>
2021-11-23 23:51:07 +0100

    input App JS and Footer JS

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <div>
+            This project was coded by Andrea Hitchin and is 
+            <a href="https://github.com/00Dre00/weather-search-react" target="_blank">
+                open-sourced on GitHub
+            </a>
+        </div>
+    );
+}
